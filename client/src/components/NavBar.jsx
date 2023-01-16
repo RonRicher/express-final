a173e1fdb087a6a8d0f83b1765ff8db323fafc45
hilma <elyasaf11@gmail.com>
2023-01-16 14:13:46 +0200

    login-signup

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -21,19 +21,9 @@ function NavBar() {
     <header id="navBar">
       <div>
         <NavLink to="/Login">LogIn</NavLink>
+        <NavLink to="/SignUp">SignUp</NavLink>
         <NavLink to="/drive">Drive</NavLink>
-        {/* <NavLink to={`users/Info`}>Info</NavLink>
-        <NavLink to={`users/Todos`}>Todos</NavLink>
-        <NavLink to={`users/Posts`}>Posts</NavLink>
-        <NavLink to={`users/Albums`}>Albums</NavLink> */}
       </div>
-      {/* {!userId && !userNum ? (
-        <NavLink to="/Login">LogIn</NavLink>
-      ) : (
-        <NavLink onClick={logOut} id="logOutButton" to="/">
-          Logout
-        </NavLink>
-      )} */}
     </header>
   );
 }
